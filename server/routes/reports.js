e8b30551ba932fb87eabe2f3a44597b06ef42493
rohanhr3452 <rohanhr3452@gmail.com>
2022-10-27 03:09:19 +0530

    Common filterMap object

--- server/routes/reports.js
+++ server/routes/reports.js
@@ -1,26 +1,37 @@
 const router = require('express').Router();
 const Report = require('../models/Report');
 
+const filterMap = {
+  y: 'end_year',
+  t: 'topic',
+  s: 'sector',
+  r: 'region',
+  src: 'source',
+  p: 'pestle',
+  c: 'country',
+};
+
 router.get('/reports', async (req, res) => {
   try {
-    let filters = {};
-    const filterMap = {
-      y: 'end_year',
-      t: 'topic',
-      s: 'sector',
-      r: 'region',
-      src: 'source',
-      p: 'pestle',
-      c: 'country',
-      city: 'city',
-    };
+    let filterQuery = {};
     Object.keys(req.query).forEach((k) => {
-      filters[filterMap[k]] = req.query[k].split(`,`);
+      filterQuery[filterMap[k]] = req.query[k].split(`,`);
     });
-    const reports = await Report.find(filters);
+    const reports = await Report.find(filterQuery);
     res.send(reports);
   } catch (err) {
-    console.log(err);
+    res.status(500).json({ error: true, message: 'Internal Server Error' });
+  }
+});
+
+router.get('/filterOptions', async (req, res) => {
+  try {
+    let filterOptions = {};
+    for (let i of Object.values(filterMap)) {
+      filterOptions[i] = await Report.distinct(i);
+    }
+    res.send([filterOptions]);
+  } catch (err) {
     res.status(500).json({ error: true, message: 'Internal Server Error' });
   }
 });
