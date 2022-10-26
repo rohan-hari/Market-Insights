f6f0e90657893bbdbc06be21142d591138d27846
rohanhr3452 <rohanhr3452@gmail.com>
2022-10-27 03:13:02 +0530

    Navbar component added

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css';
+
+export default function Navbar() {
+  return (
+    <div className='nav'>
+      <a className='logo' href='/'>
+        Market Insights
+      </a>
+    </div>
+  );
+}
